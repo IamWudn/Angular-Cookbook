be1fda76b68cb9b2229dbac8debdb53fdc37e713
a.bogonenko <a.bogonenko@z-it.ru>
2021-08-22 20:10:02 +0300

    Реализовал логирование через ngOnChanges.

--- chapter01/start_here/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
+++ chapter01/start_here/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-vc-logs',
   templateUrl: './vc-logs.component.html',
   styleUrls: ['./vc-logs.component.scss']
 })
-export class VcLogsComponent implements OnInit {
+export class VcLogsComponent implements OnInit, OnChanges {
   @Input() vName;
+  logs: string[] = [];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { vName: vNameChanges} = changes;
+    const curVal = vNameChanges.currentValue;
+    if (vNameChanges.isFirstChange()) {
+      this.logs.push(`initial version is ${curVal.trim()}`);
+    } else {
+      this.logs.push(`version changed to ${curVal.trim()}`);
+    }
   }
 
 }
